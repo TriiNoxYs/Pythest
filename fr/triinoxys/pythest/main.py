c82074f276b783c2e0a7c561b22bb06ade0bb8f3
TriiNoxYs <triinoxys@gmail.com>
2015-09-24 20:34:28 +0200

    Add function to classify numbers in order

--- fr/triinoxys/pythest/main.py
+++ fr/triinoxys/pythest/main.py
@@ -1,20 +1,61 @@
-#from utils.math import table
-#table(int(input("Quelle table voulez-vous afficher ?")), int(input("Combien de fois ?")))
+from utils.math import table
+from audioop import reverse
 
-v1 = 1.0
-v2 = 2.0
-v3 = 4.3
 
-count = input("Combien de nombres ?")
-count = int(count)
-i = 1
-while i <= count:
-    if i == 1:
-        print(v1)
-    elif i == 2:
-        print(v2)
-    elif i == 3:
-        print(v3)
-    i += 1
+choix = int(input("1.Table de multiplication \n2.Classer dans un certain ordre en donnant le nombre de nombres \n3.Classer dans un certain ordre\n"))
+if choix == 1:
+    nbrTable = int(input("Quelle table voulez-vous afficher ?\n"))
+    nbrFois  = int(input("Combien de fois faut-il multiplier %s ?\n" % nbrTable))
+    table(nbrTable, nbrFois)
     
-
+if choix == 2:
+    liste = []
+    
+    print("Dans quel ordre ?")
+    ordre = int(input("1.Croissant \n2.Decroissant\n"))
+    
+    nbrNombres = int(input("Combien de nombres ?\n"))
+    
+    nbrActuel = int(input("Premier nombre: "))
+    liste.append(nbrActuel)
+    
+    i = 1
+    while i < nbrNombres-1:
+        nbrActuel = int(input("Nombre suivant: "))
+        liste.append(nbrActuel)
+        i += 1
+    
+    nbrActuel = int(input("Dernier nombre: "))
+    liste.append(nbrActuel)
+    
+    if ordre == 1:
+        liste.sort(key=int, reverse=False)
+    elif ordre == 2:
+        liste.sort(key=int, reverse=True)
+    
+    print(liste)
+    
+if choix == 3:
+    liste = []
+    
+    print("Dans quel ordre ?")
+    ordre = int(input("1.Croissant \n2.Decroissant\n"))
+    
+    nbrActuel = int(input("Premier nombre: "))
+    liste.append(nbrActuel)
+    
+    while True:
+        rep = input("Nombre suivant: ")
+        if rep.isdigit():
+            rep = int(rep)
+            liste.append(rep)
+        else:
+            if rep == "q" or rep == "quit":
+                break;
+    
+    if ordre == 1:
+        liste.sort(key=int, reverse=False)
+    elif ordre == 2:
+        liste.sort(key=int, reverse=True)
+    
+    print(liste)
